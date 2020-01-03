7e7e9ac88ffa6e7a1e6ecdf2eeabfe29b2cd0f83
wadeepat <leaw.wadeepat@gmail.com>
2020-01-03 16:00:29 +0700

    finished

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -2,11 +2,20 @@
 
 using namespace std;
 
-//Write function findDistance() here
+double findDistance(double u,double a,double t){
+  return (u*t)+0.5*a*t*t;
+}
 
 int main(){
+  double u,a,t;
+  cout<<"Enter u = ";
+  cin>>u;
+  cout<<"Enter a = ";
+  cin>>a;
+  cout<<"Enter t = ";
+  cin>>t;
+  cout<<"s = "<<findDistance(u,a,t);
 
-  //Calling findDistance() using test cases
 
   return 0;
 }
