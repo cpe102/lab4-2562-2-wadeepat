ae351139d33d991e15a1223e924ebedbbb6b76f1
wadeepat <leaw.wadeepat@gmail.com>
2020-01-03 16:00:13 +0700

    finished

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+double FindY (int x){
+    double y =(3*pow(x,3))+(2*2.71828)+(sqrt((x*x)+1))+(pow(2,(2*x)+1));
+    return y;
+}
+
+int main(){
+    int x;
+    cout<<"Enter x = ";
+    cin>>x;
+    cout<<"Result Y = "<<FindY(x);
+    return 0;
+}
 
